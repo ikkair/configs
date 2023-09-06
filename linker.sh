789f2df35be385b3f88cb1cb9918b5240b0934af
ikkair <ikhlasul.kamal.i@gmail.com>
2023-09-06 23:47:16 +0700

    Fix linker bug

--- linker.sh
+++ linker.sh
@@ -10,19 +10,18 @@ fi
 function linker(){
     if [ -d "$DPATH/$1" ] && [ ! -L "$DPATH/$1" ]; then
          echo "$1 Path already existed"
-     elif [ -L $DPATH"/"$1 ]; then
-         ln -sf "$SPATH/$1" "$DPATH/$1"
+     elif [ -L "$DPATH/$1" ]; then
+         ln -sf "$SPATH/$1" "$DPATH/"
          echo "Symlink ${1^^} overwritten"
      else
-         ln -s "$SPATH/$1" "$DPATH/$1"
+         ln -s "$SPATH/$1" "$DPATH/"
          echo "Symlink ${1^^} created"
      fi
 }
 
-for FOLDER in "$SPATH/*/"
+for FOLDER in "$SPATH"/*/
 do
-    if [ -d "$FOLDER" ]; then
-        FNAME=$(basename "$FOLDER")
-        linker "$FNAME"
-    fi
+  FNAME=$(basename "$FOLDER")
+  linker "$FNAME"
+  echo "$FNAME"
 done
